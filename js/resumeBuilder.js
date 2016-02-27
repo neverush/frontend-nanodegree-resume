878595ce6416bf59c75cc7e7c18b63c185b2ed91
neverush <AlwysMacbook@dyn-118-139-28-108.its.monash.edu.au>
2016-02-27 15:09:24 +1100

    Create bio object.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,27 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
+ This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+var formattedName = HTMLheaderName.replace("%data%", "Alwy Christyo");
+var formattedRole = HTMLheaderRole.replace("%data%", "Machine Learning Engineer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+var skills = ["leadership", "CSS", "Web Design", "Web Development"];
+
+
+var bio = {
+    "name" : "Alwy Christyo",
+    "role" : "Machine Learning Engineer",
+    "contactInfo" : "nothing",
+    "pictureURL" : "images/fry.jpg",
+    "welcome" : "Welcome to my portfolio!",
+    "skills" : skills
+};
+
+
+
+$("#workExperience").append(bio);

56eaed955799fbd322a60f91b6b3a9df52002bd1
neverush <AlwysMacbook@dyn-118-139-28-108.its.monash.edu.au>
2016-02-27 16:47:38 +1100

    Finish Lesson 1 Reformat Helper.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,26 +2,66 @@
  This is empty on purpose! Your code to build the resume will go here.
  */
 
+var work = {
+    "jobs": [
+        {
+            "employer": "",
+            "title": "",
+            "location": "",
+            "dates": "",
+            "description": ""
+        }
+    ]
+};
 
-var formattedName = HTMLheaderName.replace("%data%", "Alwy Christyo");
-var formattedRole = HTMLheaderRole.replace("%data%", "Machine Learning Engineer");
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
+var projects = {
+    "projects": [
+        {
+            "title": "",
+            "dates": "",
+            "images": [
+                "url",
+                "url2",
+                "url3"
+            ]
+        }
+    ]
+};
 
 var skills = ["leadership", "CSS", "Web Design", "Web Development"];
 
 
 var bio = {
-    "name" : "Alwy Christyo",
-    "role" : "Machine Learning Engineer",
-    "contactInfo" : "nothing",
-    "pictureURL" : "images/fry.jpg",
-    "welcome" : "Welcome to my portfolio!",
-    "skills" : skills
+    "name": "Alwy Christyo",
+    "role": "Machine Learning Engineer",
+    "welcomeMessage": "Welcome to my portfolio!",
+
+    "contacts": {
+        "email": "",
+        "location": "Melbourne"
+    },
+    "pictureURL": "images/fry.jpg",
+    "skills": [
+        "leadership",
+        "CSS",
+        "Web Design",
+        "Web Development"
+    ]
 };
 
-
-
-$("#workExperience").append(bio);
+var education = {
+    "schools": [
+        {
+            "name": "Monash University Malaysia",
+            "city": "Kuala Lumpur",
+            "degree": "Computer Science",
+            "major": "General"
+        },
+        {
+            "name": "Monash University Australia",
+            "city": "Clayton",
+            "degree": "Bachelor of Information Technology",
+            "major": "Application Development"
+        }
+    ]
+};
